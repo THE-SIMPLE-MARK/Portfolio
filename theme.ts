a6f6ae179fb17d46ff3876707425a34b55354ef6
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2023-03-11 21:22:24 +0100

    Provide global themes to component tree

--- theme.ts
+++ theme.ts
@@ -1,4 +1,5 @@
 import { extendTheme } from "@chakra-ui/react"
+import { GlobalStyleProps, mode } from "@chakra-ui/theme-tools"
 
 const theme = extendTheme({
   config: {
@@ -7,6 +8,13 @@ const theme = extendTheme({
   fonts: {
     heading: `'Open Sans', sans-serif`,
     body: `'Open Sans', sans-serif`,
+  },
+  styles: {
+    global: (props: GlobalStyleProps) => ({
+      "html, body": {
+        bg: mode("gray.200", "gray.700")(props),
+      },
+    }),
   },
 })
 
