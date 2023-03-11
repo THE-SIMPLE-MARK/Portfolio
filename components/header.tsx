080ff0baa7899b435c6d8c4bcd769ffb02f88485
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2023-03-11 22:37:31 +0100

    Fix header mobile responsiveness

--- components/header.tsx
+++ components/header.tsx
@@ -34,7 +34,11 @@ export default function Header({ localeData }: { localeData: localeData }) {
   return (
     <>
       <Flex p="10px" w="100%" align="center">
-        <Grid alignItems="center" templateColumns="repeat(3, 1fr)" w="inherit">
+        <Grid
+          alignItems="center"
+          templateColumns={{ base: "1fr 1fr", md: "1fr auto 1fr" }}
+          w="inherit"
+        >
           <Flex justifySelf="start" gap="15px" align="center">
             <Avatar src="/logo.png" borderRadius="full" size="md" />
             <Text
@@ -42,6 +46,7 @@ export default function Header({ localeData }: { localeData: localeData }) {
               backgroundClip="text"
               fontSize="2xl"
               fontWeight="semibold"
+              whiteSpace="nowrap"
             >
               Mark Boszormenyi
             </Text>
@@ -53,6 +58,7 @@ export default function Header({ localeData }: { localeData: localeData }) {
             fontSize="2xl"
             textTransform="uppercase"
             gridColumn="2 / 3"
+            display={{ base: "none", md: "block" }}
           >
             Home
           </Text>
