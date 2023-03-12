7613a59db0472abfb1129fa48b09336bc9d44605
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2023-03-12 21:58:43 +0100

    Add Jetbrains Webstorm config files

--- components/header.tsx
+++ components/header.tsx
@@ -33,7 +33,7 @@ export default function Header({ localeData }: { localeData: localeData }) {
 
   return (
     <>
-      <Flex p="10px" w="100%" align="center">
+      <Flex pos="absolute" top="0" left="0" p="10px" w="100%" align="center">
         <Grid
           alignItems="center"
           templateColumns={{ base: "1fr 1fr", md: "1fr auto 1fr" }}
